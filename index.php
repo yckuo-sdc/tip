a75f3f255061f082175955aa0eb5659d6c11a7e7
yckuo <t910729@gmail.com>
2024-05-21 17:24:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-if (isset($_GET['sid'])) {
-    session_id($_GET['sid']);
-}	# fetch and update Session ID with sso_vision.php
 session_start();
 
 require __DIR__ . '/vendor/autoload.php';
